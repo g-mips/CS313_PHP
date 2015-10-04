635f75b6df62ee81c5997585bf9a59878bdd7f18
Grant Merrill <grant.merril@live.com>
2015-10-03 22:08:28 -0600

    added an xml file. modified the survey. modified how the results are processed and displayed.

--- individual-assignments/php_survey.php
+++ individual-assignments/php_survey.php
@@ -16,53 +16,54 @@ if (isset($_SESSION["did_survey"]) && $_SESSION["did_survey"] == true) {
 		<link rel="stylesheet" href="../css/php_survey.css">
 	</head>
 	<body>
-		<form action="" method="post">
+		<form action="results.php" method="post">
 			<h1>Gaming Survey</h1>
 			
 			<div>
 				<h3>Do you consider yourself to be a gamer?</h3>
-				<input type="radio" name="gamer" value="yes">Yes</input><br>
-				<input type="radio" name="gamer" value="no">No</input><br>
+				<input type="radio" name="gamer" value="0">Yes</input><br>
+				<input type="radio" name="gamer" value="1">No</input><br>
 				
 				<h3>What gender are you?</h3>
 				<select name="gender">
-					<option value="Male">Male</option>
-					<option value="Female">Female</option>
+					<option value="2">Male</option>
+					<option value="3">Female</option>
 				</select>
 				
 				<h3>Where do you live currently?</h3>
 				<select name="live">
-					<option value="North America">North America</option>
-					<option value="South America">South America</option>
-					<option value="Europe">Europe</option>
-					<option value="Asia">Asia</option>
-					<option value="Africa">Africa</option>
-					<option value="Austrailia">Austrailia</option>
+					<option value="4">North America</option>
+					<option value="5">South America</option>
+					<option value="6">Europe</option>
+					<option value="7">Asia</option>
+					<option value="8">Africa</option>
+					<option value="9">Australia</option>
 				</select>
 				
 				<h3>What consoles do you own currently?</h3>
-				<input type="checkbox" name="console[]" value="Xbox One">Xbox One</input><br>
-				<input type="checkbox" name="console[]" value="PlayStation 4">PlayStation 4</input><br>
-				<input type="checkbox" name="console[]" value="Wii U">Wii U</input><br>
-				<input type="checkbox" name="console[]" value="Xbox 360">Xbox 360</input><br>
-				<input type="checkbox" name="console[]" value="PlayStation 3">PlayStation 3</input><br>
-				<input type="checkbox" name="console[]" value="Wii">Wii</input><br>
-				<input type="checkbox" name="console[]" value="Xbox">Xbox</input><br>
-				<input type="checkbox" name="console[]" value="PlayStation 2">PlayStation 2</input><br>
-				<input type="checkbox" name="console[]" value="Gamecube">Gamecube</input><br>
-				<input type="checkbox" name="console[]" value="PlayStation">PlayStation</input><br>
-				<input type="checkbox" name="console[]" value="Nintendo 64">Nintendo 64</input><br>
-				<input type="checkbox" name="console[]" value="Super Nintendo">Super Nintendo</input><br>
-				<input type="checkbox" name="console[]" value="Nintendo Entertainment System">Nintendo Entertainment System</input><br>
-				<input type="checkbox" name="console[]" value="Other">Other</input><br>
+				<input type="checkbox" name="console[]" value="10">Xbox One</input><br>
+				<input type="checkbox" name="console[]" value="11">PlayStation 4</input><br>
+				<input type="checkbox" name="console[]" value="12">Wii U</input><br>
+				<input type="checkbox" name="console[]" value="13">Xbox 360</input><br>
+				<input type="checkbox" name="console[]" value="14">PlayStation 3</input><br>
+				<input type="checkbox" name="console[]" value="15">Wii</input><br>
+				<input type="checkbox" name="console[]" value="16">Xbox</input><br>
+				<input type="checkbox" name="console[]" value="17">PlayStation 2</input><br>
+				<input type="checkbox" name="console[]" value="18">Gamecube</input><br>
+				<input type="checkbox" name="console[]" value="19">PlayStation</input><br>
+				<input type="checkbox" name="console[]" value="20">Nintendo 64</input><br>
+				<input type="checkbox" name="console[]" value="21">Super Nintendo</input><br>
+				<input type="checkbox" name="console[]" value="22">Nintendo Entertainment System</input><br>
+				<input type="checkbox" name="console[]" value="23">Other</input><br>
+				<input type="checkbox" name="console[]" value="24">None</input><br>
 				
 				<h3>How many hours do you play video games a week on average?</h3>
-				<input type="radio" name="hours" value="0 hours">0 hours</input><br>
-				<input type="radio" name="hours" value="Less than 5">Less than 5</input><br>
-				<input type="radio" name="hours" value="5 - 10">5 - 10</input><br>
-				<input type="radio" name="hours" value="10 - 20">10 - 20</input><br>
-				<input type="radio" name="hours" value="20 - 30">20 - 30</input><br>
-				<input type="radio" name="hours" value="Gaming is my job">Gaming is my job</input><br>
+				<input type="radio" name="hours" value="25">0 hours</input><br>
+				<input type="radio" name="hours" value="26">Less than 5</input><br>
+				<input type="radio" name="hours" value="27">5 - 10</input><br>
+				<input type="radio" name="hours" value="28">10 - 20</input><br>
+				<input type="radio" name="hours" value="29">20 - 30</input><br>
+				<input type="radio" name="hours" value="30">Gaming is my job</input><br>
 				
 				<br>
 				
--- individual-assignments/results.php
+++ individual-assignments/results.php
@@ -2,9 +2,7 @@
 session_start();
 
 // gamer, gender, live, console, hours
-if (isset($_POST["gamer"]) && isset($_POST["gender"]) &&
-	isset($_POST["live"]) && isset($_POST["console"]) &&
-	isset($_POST["hours"])) {
+if($_SERVER['REQUEST_METHOD'] == "POST") {
 	$_SESSION["did_survey"] = true;
 } else {
 	$_SESSION["did_survey"] = false;
@@ -17,8 +15,73 @@ if (isset($_POST["gamer"]) && isset($_POST["gender"]) &&
 		<link rel="stylesheet" href="../css/php_survey.css">
 	</head>
 	<body>
-		<?php
-			if ()
+		<?php			
+			// Read XML
+			$doc = new DOMDocument();
+			$doc->load('results.xml');
+			$root = $doc->documentElement;
+			
+			// Are we requesting page with POST?
+			if($_SERVER['REQUEST_METHOD'] == "POST") {
+				// Read POST values and update XML
+				$root->childNodes->item((int)$_POST["gamer"])->nodeValue += 1;
+				$root->childNodes->item((int)$_POST["gender"])->nodeValue += 1;
+				$root->childNodes->item((int)$_POST["live"])->nodeValue += 1;
+				$root->childNodes->item((int)$_POST["hours"])->nodeValue += 1;
+				
+				$consolses = $_POST["console"];
+				foreach ($consoles as $console) {
+					$root->childNodes->item((int)$console)->nodeValue += 1;
+				}
+				
+				$doc->getElementsByTagName("numPeople")->item(0)->nodeValue += 1;
+				$doc->save('results.xml');
+			}
+			
+			// Display Results
+			echo '<h1>Results</h1>';
+			if ($doc->getElementsByTagName("numPeople")->item(0)->nodeValue == 0) {
+				echo '<div>' .
+					 '	<h3>No one has taken the survey yet. Come back later.</h3>' .
+					 '</div>';
+			} else {
+				echo '<div>' .
+					 '	<h3>This survey is currently out of' . $doc->getElementsByTagName("numPeople")->item(0)->nodeValue . '</h3>' .
+					 '	<p>' .
+					 $root->childNodes->item(0)->nodeValue . ' are gamers.<br>' .
+					 $root->childNodes->item(1)->nodeValue . ' are not gamers.<br>' .
+					 $root->childNodes->item(2)->nodeValue . ' are male.<br>' .
+					 $root->childNodes->item(3)->nodeValue . ' are female.<br>' .
+					 $root->childNodes->item(4)->nodeValue . ' live in North America.<br>' .
+					 $root->childNodes->item(5)->nodeValue . ' live in South America.<br>' .
+					 $root->childNodes->item(6)->nodeValue . ' live in Europe.<br>' .
+					 $root->childNodes->item(7)->nodeValue . ' live in Asia.<br>' .
+					 $root->childNodes->item(8)->nodeValue . ' live in Africa.<br>' .
+					 $root->childNodes->item(9)->nodeValue . ' live in Austrailia.<br>' .
+					 $root->childNodes->item(10)->nodeValue . ' own an Xbox One.<br>' .
+					 $root->childNodes->item(11)->nodeValue . ' own a PlayStation 4.<br>' .
+					 $root->childNodes->item(12)->nodeValue . ' own a Wii U.<br>' .
+					 $root->childNodes->item(13)->nodeValue . ' own an Xbox 360.<br>' .
+					 $root->childNodes->item(14)->nodeValue . ' own a PlayStation 3.<br>' .
+					 $root->childNodes->item(15)->nodeValue . ' own a Wii.<br>' .
+					 $root->childNodes->item(16)->nodeValue . ' own an Xbox.<br>' .
+					 $root->childNodes->item(17)->nodeValue . ' own a PlayStation 2.<br>' .
+					 $root->childNodes->item(18)->nodeValue . ' own a Gamecube.<br>' .
+					 $root->childNodes->item(19)->nodeValue . ' own a PlayStation.<br>' .
+					 $root->childNodes->item(20)->nodeValue . ' own a Nintendo 64.<br>' .
+					 $root->childNodes->item(21)->nodeValue . ' own a Super Nintendo.<br>' .
+					 $root->childNodes->item(22)->nodeValue . ' own a Nintendo Entertainment System.<br>' .
+					 $root->childNodes->item(23)->nodeValue . ' own a different console.<br>' .
+					 $root->childNodes->item(24)->nodeValue . ' don\'t own a console.<br>' .
+					 $root->childNodes->item(25)->nodeValue . ' play 0 hours a week.<br>' .
+					 $root->childNodes->item(26)->nodeValue . ' play less than 5 hours a week.<br>' .
+					 $root->childNodes->item(27)->nodeValue . ' play 5 - 10 hours a week.<br>' .
+					 $root->childNodes->item(28)->nodeValue . ' play 10 - 20 hours a week.<br>' .
+					 $root->childNodes->item(29)->nodeValue . ' play 20 - 30 hours a week.<br>' .
+					 $root->childNodes->item(30)->nodeValue . ' say that gaming is their job.<br>' .
+					 '   </p>' .
+					 '</div>';
+			}
 		?>
 	</body>
 </html>
