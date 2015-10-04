95cddc857b7b657ae0c267a4c84476bd700a4198
Grant Merrill <grant.merril@live.com>
2015-10-03 23:04:46 -0600

    Adjusted displaying messages.

--- individual-assignments/results.php
+++ individual-assignments/results.php
@@ -45,9 +45,26 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 					 '	<h3>No one has taken the survey yet. Come back later.</h3>' .
 					 '</div>';
 			} else {
+				$textArray = Array(' are gamers.<br>', ' are not gamers.<br>', ' are male.<br>', ' are female.<br>', ' live in North America.<br>',
+								   ' live in South America.<br>', ' live in Europe.<br>', ' live in Asia.<br>', ' live in Africa.<br>', ' live in Australia.<br>',
+								   ' own an Xbox One.<br>', ' own a PlayStation 4.<br>', ' own a Wii U.<br>', ' own an Xbox 360.<br>', ' own a PlayStation 3.<br>',
+								   ' own a Wii.<br>', ' own an Xbox.<br>', ' own a PlayStation 2.<br>', ' own a Gamecube.<br>', ' own a PlayStation.<br>',
+								   ' own a Nintendo 64.<br>', ' own a Super Nintendo.<br>', ' own a Nintendo Entertainment System.<br>', ' own a different console.<br>',
+								   ' don\'t own a console.<br>', ' play 0 hours a week.<br>', ' play less than 5 hours a week.<br>', ' play 5 - 10 hours a week.<br>',
+								   ' play 10 - 20 hours a week.<br>', ' play 20 - 30 hours a week.<br>', ' say that gaming is their job.<br>');
 				echo '<div>' .
 					 '	<h3>This survey is currently out of ' . $doc->getElementsByTagName("numPeople")->item(0)->nodeValue . ' person(s).</h3>' .
-					 '	<p>' .
+					 '	<p>';
+					
+				$textIndex = 0;
+				foreach($root->childNodes as childNode) {
+					$childNode->nodeValue . $textArray[$textIndex];
+				}
+				
+				echo '  </p>' .
+					 '</div>';
+				
+					/*
 					 $root->childNodes->item(0)->nodeValue . ' are gamers.<br>' .
 					 $root->childNodes->item(1)->nodeValue . ' are not gamers.<br>' .
 					 $root->childNodes->item(2)->nodeValue . ' are male.<br>' .
@@ -57,7 +74,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 					 $root->childNodes->item(6)->nodeValue . ' live in Europe.<br>' .
 					 $root->childNodes->item(7)->nodeValue . ' live in Asia.<br>' .
 					 $root->childNodes->item(8)->nodeValue . ' live in Africa.<br>' .
-					 $root->childNodes->item(9)->nodeValue . ' live in Austrailia.<br>' .
+					 $root->childNodes->item(9)->nodeValue . ' live in Australia.<br>' .
 					 $root->childNodes->item(10)->nodeValue . ' own an Xbox One.<br>' .
 					 $root->childNodes->item(11)->nodeValue . ' own a PlayStation 4.<br>' .
 					 $root->childNodes->item(12)->nodeValue . ' own a Wii U.<br>' .
@@ -79,8 +96,10 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 					 $root->childNodes->item(28)->nodeValue . ' play 10 - 20 hours a week.<br>' .
 					 $root->childNodes->item(29)->nodeValue . ' play 20 - 30 hours a week.<br>' .
 					 $root->childNodes->item(30)->nodeValue . ' say that gaming is their job.<br>' .
+					 
 					 '   </p>' .
 					 '</div>';
+					 */
 			}
 		?>
 	</body>
