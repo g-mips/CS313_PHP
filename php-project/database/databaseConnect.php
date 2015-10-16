4d58b79c44453d4174d2aa17d454986cffeb4021
Grant Merrill <grant.merril@live.com>
2015-10-16 16:05:21 -0600

    Added a database Connection PHP file. Testing file in forumContent.html.

---
+++ php-project/database/databaseConnect.php
@@ -0,0 +1,24 @@
+<?php
+
+function loadDatabase() { 
+    $dbHost = "";
+    $dbPort = "";
+    $dbUser = "";
+    $dbPassword = ""; 
+
+    $dbName = "testdb"; 
+
+    $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
+
+    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n"; 
+
+//    $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword); 
+
+    return $db;
+} 
+
+?> 
