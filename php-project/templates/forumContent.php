efb3cafa1d5404fa9898c785ad305967b4462800
Grant Merrill <grant.merril@live.com>
2015-10-17 12:03:44 -0600

    Restructued forumContent.php a bit

--- php-project/templates/forumContent.php
+++ php-project/templates/forumContent.php
@@ -9,15 +9,19 @@
                 
                 // Categories Loop
                 foreach ($cats as $cat) {
-                    echo "<h1 class='ForumCat'>" . $cat["cat_name"] . "</h1>";
+                    echo "<section class='Cat'>";
+                    echo "<h1 class='CatName'>" . $cat["cat_name"] . "</h1>";
                     
                     $sub_cats = $db->query("SELECT * FROM sub_categories INNER JOIN categories ON sub_categories.sub_cat_cat = categories.cat_id WHERE categories.cat_id = " . $cat["cat_id"] . " ORDER BY sub_categories.sub_cat_order");
                     
                     foreach ($sub_cats as $sub_cat) {
-                        echo "<h2 class='ForumSubCat'>" . $sub_cat["sub_cat_name"] . "</h2>";
+                        echo "<section class='SubCat'>"
+                        echo "<h2 class='SubCatName'>" . $sub_cat["sub_cat_name"] . "</h2>";
                         echo "<p class='SubCatDescription'>" . $sub_cat["sub_cat_description"] . "</p>";
                         echo "<hr class='ForumLine'/>";
+                        echo "</section>";
                     }
+                    echo "</section>";
                 }
             }
     ?>
