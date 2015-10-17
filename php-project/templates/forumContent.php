47e5026b8ea87c27a5a0233549acf3b4643ae1a7
Grant Merrill <grant.merril@live.com>
2015-10-17 16:30:40 -0600

    Checking my PHP code for errors

--- php-project/templates/forumContent.php
+++ php-project/templates/forumContent.php
@@ -43,7 +43,7 @@
             } else if ($_SESSION['id'] !== null) {
                 if ($_SESSION['page'] == 1) {
                     $cat = $db->query("SELECT * FROM categories WHERE cat_id = " . $_SESSION['id']);
-                    echo "<h1 class='ForumTitle'>" . $cat["cat_name"] . "</h1>";
+                    //echo "<h1 class='ForumTitle'>" . $cat["cat_name"] . "</h1>";
 
                     //displaySubCats($cat);
                 } else if ($_SESSION['page'] == 2) {
