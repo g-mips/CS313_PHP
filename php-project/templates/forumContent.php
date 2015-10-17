75433b60631bf27b1aae8d3a7e53b7582d344369
Grant Merrill <grant.merril@live.com>
2015-10-17 17:58:29 -0600

    Checking my PHP code for errors

--- php-project/templates/forumContent.php
+++ php-project/templates/forumContent.php
@@ -6,9 +6,8 @@
         require "../database/databaseConnect.php";
 
         function displaySubCats($db, $id) {
-            var_dump($id);
             $sub_cats = $db->query("SELECT * FROM sub_categories INNER JOIN categories ON sub_categories.sub_cat_cat = categories.cat_id WHERE categories.cat_id = " . $id . " ORDER BY sub_categories.sub_cat_order");
-            /*
+            
             foreach ($sub_cats as $sub_cat) {
                 echo "<a class='ForumLink' href=''>";
                 echo "<section class='SubCat'>";
@@ -17,7 +16,7 @@
                 echo "<hr class='ForumLine'/>";
                 echo "</section>";
                 echo "</a>";
-            }*/
+            }
         }
 
         function run() {
