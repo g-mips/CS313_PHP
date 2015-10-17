29206b87750336577b736c906b68f6322c9d1c47
Grant Merrill <grant.merril@live.com>
2015-10-17 10:27:06 -0600

    Fixed syntax error in SELECT statements.

--- php-project/templates/forumContent.php
+++ php-project/templates/forumContent.php
@@ -4,12 +4,12 @@
             $db = loadDatabase();
 
             if ($db !== null) {
-                $cats = $db->query("SELECT * FROM categories ORDER BY order");
+                $cats = $db->query("SELECT * FROM categories ORDER BY cat_order");
                 
                 foreach ($cats as $cat) {
                     echo "<h1 class='ForumCat'>" . $cat["cat_name"] . "</h1>";
                     
-                    $sub_cats = $db->query("SELECT * FROM sub_categories INNER JOIN categories ON sub_categories.sub_cat_cat = categories.cat_id WHERE categories.cat_id = " . $cat["cat_name"] . " ORDER BY sub_categories.order");
+                    $sub_cats = $db->query("SELECT * FROM sub_categories INNER JOIN categories ON sub_categories.sub_cat_cat = categories.cat_id WHERE categories.cat_id = " . $cat["cat_name"] . " ORDER BY sub_categories.sub_cat_order");
                     
                     foreach ($sub_cats as $sub_cat) {
                         echo "<h2 class='ForumSubCat'>" . $sub_cat["sub_cat_name"] . "</h2>";
