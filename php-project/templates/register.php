adbdf1a81b572b99d1092613bf9ad5d09a1d5977
Grant Merrill <grant.merril@live.com>
2015-10-21 16:53:06 -0600

    Added code to check against existing username and email

--- php-project/templates/register.php
+++ php-project/templates/register.php
@@ -3,32 +3,58 @@
         $result = "";
         
         if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email"]) && isset($_POST["cPassword"])) {
-            $pass  = sha1($_POST["password"]);
-            $user  = $_POST["username"];
-            $email = $_POST["email"];
-            
-            if (!ctype_alnum($user)) {
+            if (!ctype_alnum($_POST["username"])) {
                 $result = "Username can only contain numbers and/or letters.";
-            } else if (strlen($user) > 30) {
+            } else if (strlen($_POST["username"]) > 30) {
                 $result = "Username cannot exceed 30 characters.";
             } else if ($_POST["password"] !== $_POST["cPassword"]) {
                 $result = "Passwords do not match.";
             } else {
                 require ('/php-project/database/databaseConnect.php');
                 try {
+                    console.log("LOADING DB");
                     $db = loadDatabase();
+                    console.log("DB LOADED");
+                    
+                    $exists = $db->query("SELECT user_name FROM users WHERE user_name = '" . $_POST["username"] . "' LIMIT 1");
+                    if ($exists === false || $exists->rowCount === 1) {
+                        $exists = $mysqli->query("SELECT email from users WHERE email = '" . $_POST["email"] . "' LIMIT 1");
+                        if ($exists === false || $exists->num_rows === 1) {
+                            $result = "Username and Email already exist!";
+                        } else {
+                            $result = "Username already exist!";
+                        }
+                    } else {
+                        $exists = $db->query("SELECT user_email FROM users WHERE user_email = '" . $_POST["email"] . "' LIMIT 1");
+                        if ($exists === false || $exists->rowCount === 1) {
+                            $result = "Email is already bound to a Username.";
+                        } else {
+                            $statement = $db->prepare("INSERT INTO users (user_date, user_email, user_name, user_pass, user_type) VALUES(?, ?, ?, ?, ?)");
+                            console.log("STATEMENT PREPARED");
 
-                    $statement = $db->prepare("INSERT INTO users (user_date, user_email, user_name, user_pass, user_type) VALUES(?, ?, ?, ?, ?)");
+                            $pass  = null;
+                            $user  = null;
+                            $email = null;
+                            $date  = null;
+                            $type  = null;
 
-                    $statement->bindParam(1, 'NOW()');
-                    $statement->bindParam(2, $email);
-                    $statement->bindParam(3, $user);
-                    $statement->bindParam(4, $pass);
-                    $statement->bindParam(5, 0);
+                            $statement->bindParam(1, $date);
+                            $statement->bindParam(2, $email);
+                            $statement->bindParam(3, $user);
+                            $statement->bindParam(4, $pass);
+                            $statement->bindParam(5, $type);
 
-                    $statement->exec();
+                            $pass  = sha1($_POST["password"]);
+                            $user  = $_POST["username"];
+                            $email = $_POST["email"];
+                            $date = 'NOW()';
+                            $type = 0;
 
-                    $result = "User Registered!";
+                            $statement->exec();
+
+                            $result = "User Registered!";
+                        }
+                    }
                 } catch (PDOException $e) {
                     $result = $e->getMessage();
                 }
