c550e9c14784836da3a67f40058d43acebaac887
Grant Merrill <grant.merril@live.com>
2015-10-21 13:38:36 -0600

    Added ng-models to my form inputs.

--- php-project/js/signup.js
+++ php-project/js/signup.js
@@ -2,11 +2,15 @@
     kacologoApp = angular.module('kacologo-signup', []);
     
     /**************************** Signup Page Controller ****************************/
-    kacologoApp.controller('SignupCtrl', function() {
+    kacologoApp.controller('SignupCtrl', ['$scope', function($scope) {
+        $scope.username = "";
+        $scope.password = "";
+        $scope.email = "";
+        
         var register = function() {
             
         };
-    });
+    }]);
     
     kacologoApp.directive('signupContent', function() {
         return {
