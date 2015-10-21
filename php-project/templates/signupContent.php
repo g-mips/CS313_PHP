0909b4d6b0f950fca748036541336464b745a028
Grant Merrill <grant.merril@live.com>
2015-10-21 14:25:24 -0600

    Fixed bug of same named variables

--- php-project/templates/signupContent.php
+++ php-project/templates/signupContent.php
@@ -1,10 +1,10 @@
 <!--action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>"-->
-<form name="signup" ng-submit="signup.$valid && signup.register()" method="post" novalidate>
+<form name="signupForm" ng-submit="signupForm.$valid && signup.register()" method="post" novalidate>
     Username: <input ng-model="username" type="text" name="username" required/><br />
     Password: <input ng-model="password" type="password" name="password" required/><br />
     Email: <input ng-model="email" type="email" name="email" required/><br />
 
-    <input type="submit" name="submit" value="Register" ng-disabled="signup.$invalid" />
+    <input type="submit" name="submit" value="Register" ng-disabled="signupForm.$invalid" />
 </form>
 
 <section>{{submissionResult}}</section>
