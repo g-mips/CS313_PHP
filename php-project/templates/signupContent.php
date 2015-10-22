7eda3778f3c21699b46959677669549a51525017
Grant Merrill <grant.merril@live.com>
2015-10-21 19:27:53 -0600

    Testing css styling on form.

--- php-project/templates/signupContent.php
+++ php-project/templates/signupContent.php
@@ -1,19 +1,19 @@
 <!--action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>"-->
 <form name="signupForm" ng-submit="signupForm.$valid && register()" novalidate>
     <div>
-        <label for="UserName">Username:</label>
+        <label for="UserName">Username </label>
         <input id="UserName" ng-model="username" type="text" name="username" required/><br />
     </div>
     <div>
-        <label for="Password">Password:</label>
+        <label for="Password">Password </label>
         <input id="Password" ng-model="password" type="password" name="password" required/><br />
     </div>
     <div>
-        <label for="CPassword">Confirm Password:</label>
+        <label for="CPassword">Confirm Password </label>
         <input id="CPassword" ng-model="cPassword" type="password" name="cpassword" required/><br />
     </div>
     <div>
-        <label id="Email">Email:</label>
+        <label id="Email">Email </label>
         <input ng-model="email" type="email" name="email" required/><br />
     </div>
     <div>
