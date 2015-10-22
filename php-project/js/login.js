462652eebf5b3cc4a8f1a37177edef7e08ecf081
Grant Merrill <grant.merril@live.com>
2015-10-21 23:20:22 -0600

    Testing

--- php-project/js/login.js
+++ php-project/js/login.js
@@ -16,7 +16,7 @@
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             }).success(function (data, status, headers, config)
                 {
-                    $log.log("DATA: " + data + " :DATA);
+                    $log.log("DATA: " + data + " :DATA");
                     if (data == 'SUCCESS') {
                         data += " YO";
                         document.location.href = 'http://php-gshawm.rhcloud.com/php-project/php_index.html';
