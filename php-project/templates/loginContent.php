3aceaa6c2e5c94730359bb80e5d6f9959365337d
Grant Merrill <grant.merril@live.com>
2015-10-21 21:20:52 -0600

    Changed password type to password. Change Login() to login() in ng-submit.

--- php-project/templates/loginContent.php
+++ php-project/templates/loginContent.php
@@ -1,13 +1,13 @@
 <h1 class="Title">Login</h1>
 
-<form name="loginForm" ng-submit="loginForm.$valid && Login()" novalidate>
+<form name="loginForm" ng-submit="loginForm.$valid && login()" novalidate>
     <div>
         <label for="Username">Username </label>
         <input id="Username" ng-model="username" type="text" name="username" required/><br />
     </div>
     <div>
         <label for="Password">Password </label>
-        <input id="Password" ng-model="password" type="text" name="password" required/><br />
+        <input id="Password" ng-model="password" type="password" name="password" required/><br />
     </div>
     <div>
         <input id="Submit" type="submit" name="submit" value="Login" ng-disabled="loginForm.$invalid" />
