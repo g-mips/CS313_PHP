9d59c7b842ccf23bde8b3ee9944c2212488ae96e
Grant Merrill <grant.merril@live.com>
2015-10-22 19:45:51 -0600

    testing

--- php-project/templates/profileContent.php
+++ php-project/templates/profileContent.php
@@ -1,6 +1,14 @@
 <?php
     session_start();
+    require ('../database/databaseConnect.php');
+    $db = loadDatabase();
+    $test = "HEY";
 ?>
 <section>
     <h1 class="Title"><?php echo $_SESSION['user'] . "'s Profile"; ?></h1>
+    
+    <div>
+        <div><?php echo $test ?></div>
+        <div></div>
+    </div>
 </section>
