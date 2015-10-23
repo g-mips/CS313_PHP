8da9145493390d14c3fbb1e9ff3c05eeabaa503b
Grant Merrill <grant.merril@live.com>
2015-10-23 03:13:33 -0600

    Debug

--- php-project/templates/changePassword.php
+++ php-project/templates/changePassword.php
@@ -2,7 +2,7 @@
     session_start();
     require ('../database/password.php');
 
-    $pass = password_hash($_POST["nPassword"], PASSWORD_DEFAULT);
+    password_hash($_POST["nPassword"], PASSWORD_DEFAULT);
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $result = "";
 
