0ecf89d54eaaf28e02a12c8a3e6d71b4a728f97f
Grant Merrill <grant.merril@live.com>
2015-10-22 20:03:56 -0600

    Added database code to profile page.

--- php-project/templates/profileContent.php
+++ php-project/templates/profileContent.php
@@ -1,14 +1,26 @@
 <?php
     session_start();
-    require ('../database/databaseConnect.php');
-    $db = loadDatabase();
-    $test = "HEY";
+    if ($_SESSION['user'] !== null) {
+        require ('../database/databaseConnect.php');
+        $db = loadDatabase();
+        $user = $db->query("SELECT * FROM users WHERE user_name = '" . $_SESSION['user'] . "' LIMIT 1");
+
+        if ($user !== false && $user->rowCount === 1) {
+            $user = $user->fetch();
+        } else {
+            header('Location: http://php-gshawm.rhcloud.com/php-project/php_index.php');
+            exit();
+        }
+    } else {
+        header('Location: http://php-gshawm.rhcloud.com/php-project/php_index.php');
+        exit();
+    }
 ?>
 <section>
-    <h1 class="Title"><?php echo $_SESSION['user'] . "'s Profile"; ?></h1>
+    <h1 class="Title"><?php echo $user["user_name"] . "'s Profile"; ?></h1>
     
     <div>
-        <div><?php echo $test ?></div>
+        <div></div>
         <div></div>
     </div>
 </section>
