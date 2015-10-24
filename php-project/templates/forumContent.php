e0a6e45aad8f4a8534c8bfa4796a4b88b8d91da4
Grant Merrill <grant.merril@live.com>
2015-10-24 17:21:38 -0600

    Testing navigation bar for forum

--- php-project/templates/forumContent.php
+++ php-project/templates/forumContent.php
@@ -62,8 +62,7 @@
                         echo "CUR: " . $curTable . "<br />";
                         echo "PRE: " . $preTable . "<br />";
                         
-                        $query = "SELECT * FROM " . $tables[$preTable] . " INNER JOIN " . $tables[$curTable] . " ON " . $tables[$curTable] .
-                            "." . tableFks[$curTable] . " = " . $tables[$preTable] . "." . $tableIds[$preTable];
+                        $query = "SELECT * FROM " . $tables[$preTable] . " INNER JOIN " . $tables[$curTable] . " ON " . $tables[$curTable] . "." . tableFks[$curTable] . " = " . $tables[$preTable] . "." . $tableIds[$preTable];
                         
                         echo $query . "<br />";
                         //$results = $db->query($query);
