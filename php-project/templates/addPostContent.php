50a4455d5b6f701689eb7960d9424f5cbbbab606
Grant Merrill <grant.merril@live.com>
2015-10-25 02:23:25 -0600

    Styled

--- php-project/templates/addPostContent.php
+++ php-project/templates/addPostContent.php
@@ -2,12 +2,13 @@
     session_start();
 ?>
 <form name="addPostForm" ng-submit="addPostForm.$valid" novalidate>
-    <div class="FullWidth">
+    <div class="FormDiv">
         <label for="Subject">Subject</label>
         <input id="Subject" ng-model="subject" type="text" name="subject" requred/><br />
     </div>
-    <div class="FullWidth">
-        <textarea rows="30" cols="70" required></textarea>
+    <div class="FormDiv">
+        <label for="Content">Content</label>
+        <textarea id="Content" rows="30" cols="70" required></textarea>
     </div>
     <div>
         <input id="Submit" type="submit" name="submit" value="Add Post" ng-disabled="addPostForm.$invalid" />
