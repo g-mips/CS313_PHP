6f39fd0c3d629b9256fcb9611eb9bbe9e2e65829
Grant Merrill <grant.merril@live.com>
2015-10-25 01:21:03 -0600

    Added a add post button

---
+++ php-project/addPost.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html ng-app="kacologoApp">
+	<head>
+        <title>Kacologo</title>
+        <link rel="stylesheet" href="css/main.css" />
+        <link rel="stylesheet" href="css/nav.css" />
+        <link rel="stylesheet" href="css/footer.css" />
+
+        <link rel="shortcut icon" href="images/controller-small.png">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
+        <script src="js/main.js"></script>
+        <script src="js/home.js"></script>
+        <script src="js/videos.js"></script>
+        <script src="js/forum.js"></script>
+        <script src="js/contact.js"></script>
+        <script src="js/signup.js"></script>
+        <script src="js/login.js"></script>
+        <script src="js/profile.js"></script>
+    </head>
+	<body ng-controller="MainCtrl as main" ng-init="main.page = 'ADDPOST'">
+        <navigator></navigator>
+        <content></content>
+        <footer-links></footer-links>
+	</body>
+</html>
