2ec43373f8306dbce118bf4cf7b22f9b877765f1
Grant Merrill <grant.merril@live.com>
2015-10-25 03:28:13 -0600

    Added MySQL INSERT statements for the topic and post

--- php-project/templates/addThreadOrPost.php
+++ php-project/templates/addThreadOrPost.php
@@ -2,8 +2,40 @@
     session_start();
 
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        if (isset($_POST["subject"]) && isset($_POST["content"]) && isset($_SESSION['id'])) {
-            echo "HI";
+        if (isset($_POST["subject"]) && isset($_POST["content"]) && isset($_SESSION['id']) && $_SESSION['logged']) {
+            require ('../database/databaseConnect.php');
+            
+            $db = loadDatabase();
+            
+            $statement = $db->prepare("INSERT INTO topics (topic_author, topic_date, topic_pinned, topic_subject, topic_sub_cat) VALUES (?, NOW(), ?, ?, ?)");
+            $author = null;
+            $pinned = null;
+            $subject = null;
+            $subCat = null;
+            
+            $statement->bindParam(1, $author);
+            $statement->bindParam(2, $pinned);
+            $statement->bindParam(3, $subject);
+            $statement->bindParam(4, $subCat);
+            
+            $author = $_SESSION['user'];
+            $pinned = false;
+            $subject = $_POST["subject"];
+            $subCat = $_SESSION['id'];
+            
+            $statement->execute();
+            
+            $id = $db->lastInsertId();
+            
+            $statement = $db->prepare("INSERT INTO posts (post_author, post_content, post_date, post_topic) VALUES (?, ?, NOW(), ?)");
+            
+            $statement->bindParam(1, $author);
+            $statement->bindParam(2, $_POST["content"]);
+            $statement->bindParam(3, $id);
+            
+            $statement->execute();
+            
+            echo "SUCCESS";
         }
     }
 ?>
