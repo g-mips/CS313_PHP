72eae9c84e884851d2baa44a749368d616c6874f
Grant Merrill <grant.merril@live.com>
2015-10-31 11:52:19 -0600

    Fixing textarea problem

--- php-project/templates/addThreadContent.php
+++ php-project/templates/addThreadContent.php
@@ -5,11 +5,10 @@
     <h1 class="Title">Add Thread</h1>
     <form name="addPostForm" ng-submit="addPostForm.$valid && addThread()" novalidate>
         <div class="FormDiv">
-            <label for="Subject">Subject</label>
-            <input id="Subject" ng-model="subject" type="text" name="subject" requred/><br />
+            <input id="Subject" ng-model="subject" type="text" name="subject" placeholder="Subject" requred/><br />
         </div>
         <div class="FormDiv">
-            <textarea id="Content" ng-model="content" name="content" rows="30" cols="70" required></textarea>
+            <textarea id="Content" ng-model="content" name="content" rows="30" cols="70" placeholder="Content" required></textarea>
         </div>
         <div class="FormDiv">
             <label></label>
